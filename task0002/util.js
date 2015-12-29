4d591780c9e11250cb9b1526ab64a41902ffa967
Wangjing <lenghai225@gmail.com>
2015-12-29 22:43:52 +0800

    3

--- task0002/util.js
+++ task0002/util.js
@@ -85,5 +85,86 @@ console.log(b); // [1, 3, 5, 7]
 // 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和
 //尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
 function simpleTrim(str) {
-    // your implement
+    while(str.charAt(0) == " ") {
+        str = str.substr(1);
+    }
+    while(str.charAt(str.length-1) == " ") {
+        str = str.slice(0,-2);
+    }
+    return str;
 }
+
+//测试用例
+var mystr = "   wo s s   ";
+var b = simpleTrim(mystr);
+console.log(b);
+
+
+
+
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    var newStr = str.replace(/^\s+|\s+$/g,"");
+    return newStr;
+}
+
+// 使用示例
+var str = '   hi!  ';
+str = trim(str);
+console.log(str); // 'hi!'
+
+
+
+
+
+
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn) {
+    for(var i = 0;i<arr.length;i++) {
+        fn(arr[i],i);
+    }
+}
+
+// 其中fn函数可以接受两个参数：item和index
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item) {
+    console.log(item)
+}
+each(arr, output);  // java, c, php, html
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item, index) {
+    console.log(index + ': ' + item)
+}
+each(arr, output);  // 0:java, 1:c, 2:php, 3:html
+
+
+
+
+
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    var count = 0;
+    for(var k in obj) {
+        if(obj.hasOwnProperty(k)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// 使用示例
+var obj = {
+    a: 1,
+    b: 2,
+    c: {
+        c1: 3,
+        c2: 4
+    }
+};
+console.log(getObjectLength(obj)); // 3
